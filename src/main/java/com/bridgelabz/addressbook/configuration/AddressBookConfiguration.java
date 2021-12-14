198850c4241279da3e23d1414c56eae202ca80e0
kunal <43981189+kunalsuryawanshi@users.noreply.github.com>
2021-12-14 16:40:16 +0530

    Added: CRUD Operations for AddressBook in Service Class and Created MySql Database for Store AddressBook Data

---
+++ src/main/java/com/bridgelabz/addressbook/configuration/AddressBookConfiguration.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbook.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.modelmapper.ModelMapper;
+
+@Configuration
+public class AddressBookConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
---
+++ src/main/java/com/bridgelabz/addressbook/repository/AddressBookRepository.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbook.repository;
+
+import com.bridgelabz.addressbook.entity.AddressBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressBookRepository extends JpaRepository<AddressBook, Integer> {
+}
